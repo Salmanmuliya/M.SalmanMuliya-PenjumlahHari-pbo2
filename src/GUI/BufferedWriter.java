25cbb408018d972b8db562aff002a53da1c6dcf9
Salmanmuliya <140207946+Salmanmuliya@users.noreply.github.com>
2023-11-13 14:07:48 +0800

    Add files via upload

---
+++ src/GUI/BufferedWriter.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.io.FileWriter;
+
+/**
+ *
+ * @author asus
+ */
+class BufferedWriter {
+
+    BufferedWriter(FileWriter fileWriter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void write(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
